8d483da00b48d1ada58a1e966c58a574dc2732b0
willianZatta <s.abner@aluno.ifsp.edu.br>
2024-09-24 20:22:41 -0300

    promto
    pronto

--- view/Home/NovoAgendamento.js
+++ view/Home/NovoAgendamento.js
@@ -1,10 +1,11 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import { FloatingLabelInput } from 'react-native-floating-label-input';
+
 import { StyleSheet, Text, View, Button, TextInput, Pressable} from "react-native";
 import * as Yup from 'yup';
 import { Formik } from 'formik'; 
 import adicionarAgendamento from "../../services/agendamentoService";
-import CriarAgendamento from '../../database/agendamentoRepository';
 
 const Validation = Yup.object().shape({
   Nome: Yup.string()
@@ -12,16 +13,13 @@ const Validation = Yup.object().shape({
     .max(50, 'Máximo de 50 caracteres!')
     .required('Obrigatorio'),
     
-  // Telefone: Yup.number()
-  //   .typeError('Apenas números inteiros')
-  //   .positive('O numero tem que ser positivo')
-  //   .min(10000000000, 'Minimo de 11 caracteres!')
-  //   .max(999999999999, 'Máximo de 11 caracteres!')
-  //   .required('Obrigatorio'),
+    Telefone: Yup.string()
+    .matches(/^\d{10,11}$/, 'O telefone deve ter 10 ou 11 dígitos e conter apenas números')
+    .required('Obrigatório'),
 
-  //   Data: Yup.string()
-  //   .matches(/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/, 'A data deve estar no formato DD/MM/YYYY.')
-  //   .required('Campo obrigatório.'),
+    Data: Yup.string()
+    .matches(/^\d{4}-\d{2}-\d{2}$/, 'A data deve estar no formato DD/MM/YYYY.')
+    .required('Campo obrigatório.'),
   
   // Hora: Yup.string()
   //   .matches(/^([01]\d|2[0-3]):([0-5]\d)$/, 'A hora deve estar no formato HH:mm.')
@@ -47,40 +45,36 @@ async function criarAgendamento(fecharModal, nome, telefone, data, hora, prestad
       prestador,
       servico,
     };
-    console.log("Agendamento mockado:", agendamento);
-    await adicionarAgendamento(agendamento);
-    console.log("Sucesso", "Agendamento cadastrado com sucesso!");
+    console.log("Agendamento:", agendamento);
+    var res = await adicionarAgendamento(agendamento);
+    console.log("Resposta:", res);
+    // console.log("Sucesso", "Agendamento cadastrado com sucesso!");
     fecharModal();
   } catch (error) {
-    console.error("Erro ao inserir o agendamento mockado:", error);
+    console.error("Erro ao inserir o agendamento:", error);
+    
   }
 }
 
 
 
 export default function NovoAgendamento({ fecharModal }) {
-  const inputRefs = useRef({});
-
-  const focusInput = (field) => {
-    inputRefs.current[field]?.focus();
-  };
-
   const [date, setDate] = useState(new Date());
   const [show, setShow] = useState(false);
-  const [DateString, setDateString] = useState('');
+  const [DateString, setDateString] = useState(date.toISOString().split('T')[0]);
 
 
   const onChange = (event, selectedDate) => {
     const currentDate = selectedDate || date;
     setShow(false);
     setDate(currentDate);
-    setDateString(currentDate.toLocaleDateString('pt-BR'));
+    setDateString(currentDate.toISOString().split('T')[0]);
   };
 
   //time
   const [time, setTime] = useState(new Date());  
   const [showtime, setShowtime] = useState(false);
-  const [timeString, setTimeString] = useState('');
+  const [timeString, setTimeString] = useState(time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
 
   const onChangeTime = (event, selectedTime) => {
     const currentTime = selectedTime || time;
@@ -107,37 +101,35 @@ export default function NovoAgendamento({ fecharModal }) {
 
     >
       {({ handleChange, handleBlur, handleSubmit, errors, touched, values }) => (
-        <View style={styles.container}>  
-          <Pressable onPress={() => focusInput('Nome')}>
-            <Text style={styles.label}>Nome</Text>
-          </Pressable>        
-          <TextInput
-            ref={(ref) => (inputRefs.current.Nome = ref)}
-            style={styles.input}
-            onChangeText={handleChange('Nome')}
-            onBlur={handleBlur('Nome')}
-            value={values.Nome}
-            placeholder="Nome"
-          />
-          {errors.Nome && touched.Nome ? (
-             <Text style={styles.error}>{errors.Nome}</Text>
-           ) : null}
-          
-          <Pressable onPress={() => focusInput('Telefone')}>
-            <Text style={styles.label}>Telefone</Text>
-          </Pressable>        
-          <TextInput
-            ref={(ref) => (inputRefs.current.Telefone = ref)}
-            style={styles.input}
-            onChangeText={handleChange('Telefone')}
-            onBlur={handleBlur('Telefone')}
-            value={values.Telefone}
-            placeholder="exemplo: 17997556141"
-            keyboardType="numeric"
-          />
-          {errors.Telefone && touched.Telefone ? (
-             <Text style={styles.error}>{errors.Telefone}</Text>
-           ) : null}
+
+        
+        <View style={styles.container}> 
+          <View style={styles.inputContainer}>
+            <FloatingLabelInput
+              style={styles.input}
+              labelStyles={{ backgroundColor: "white", paddingHorizontal:10}}
+              onChangeText={handleChange('Nome')}
+              value={values.Nome}
+              label="Nome"
+            />
+            {errors.Nome && touched.Nome ? (
+              <Text style={styles.error}>{errors.Nome}</Text>
+            ) : null}
+          </View>
+
+          <View style={styles.inputContainer}>          
+            <FloatingLabelInput
+              style={styles.input}
+              labelStyles={{ backgroundColor: "white", paddingHorizontal:10}}
+              onChangeText={handleChange('Telefone')}
+              value={values.Telefone}
+              label="Telefone"
+              keyboardType="numeric"
+            />
+            {errors.Telefone && touched.Telefone ? (
+              <Text style={styles.error}>{errors.Telefone}</Text>
+            ) : null}
+          </View>
 
 
           <Button title="Data" onPress={() => setShow(true)} />
@@ -154,7 +146,6 @@ export default function NovoAgendamento({ fecharModal }) {
             <TextInput
             style={styles.input}
             onChangeText={handleChange('date')}
-            onBlur={handleBlur('date')}
             value={date.toLocaleDateString()}
             editable={false} // Impedir edição manual
           />
@@ -169,45 +160,38 @@ export default function NovoAgendamento({ fecharModal }) {
               onChange={onChangeTime}
             />
           )}
-          <TextInput
-            style={styles.input}
-            onChangeText={handleChange('Hora')}
-            onBlur={handleBlur('Hora')}
-            value={time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
-            placeholder="Hora"
-            editable={false}
-          />
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              onChangeText={handleChange('Hora')}
+              value={time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+              editable={false}
+            />
+          </View>
 
-
-          <Pressable onPress={() => focusInput('Prestador')}>
-            <Text style={styles.label}>Prestador</Text>
-          </Pressable>        
-          <TextInput
-            ref={(ref) => (inputRefs.current.Prestador = ref)}
-            style={styles.input}
-            onChangeText={handleChange('Prestador')}
-            onBlur={handleBlur('Prestador')}
-            value={values.Prestador}
-            placeholder="Prestador"
-          />
+          <View style={styles.inputContainer}>
+            <FloatingLabelInput
+              labelStyles={{ backgroundColor: "white", paddingHorizontal:10}}
+              onChangeText={handleChange('Prestador')}
+              value={values.Prestador}
+              label="Prestador"
+            />
           {errors.Prestador && touched.Prestador ? (
-             <Text style={styles.error}>{errors.Prestador}</Text>
-           ) : null}
+            <Text style={styles.error}>{errors.Prestador}</Text>
+          ) : null}
+          </View>
 
-          <Pressable onPress={() => focusInput('Servico')}>
-            <Text style={styles.label}>Servico</Text>
-          </Pressable>        
-          <TextInput
-            ref={(ref) => (inputRefs.current.Servico = ref)}
-            style={styles.input}
-            onChangeText={handleChange('Servico')}
-            onBlur={handleBlur('Servico')}
-            value={values.Servico}
-            placeholder="Serviço"
-          />
-          {errors.Servico && touched.Servico ? (
-             <Text style={styles.error}>{errors.Servico}</Text>
-           ) : null}
+          <View style={styles.inputContainer}>
+            <FloatingLabelInput
+              labelStyles={{ backgroundColor: "white", paddingHorizontal:10,}}
+              onChangeText={handleChange('Servico')}
+              value={values.Servico}
+              label="Servico"
+            />
+            {errors.Servico && touched.Servico ? (
+              <Text style={styles.error}>{errors.Servico}</Text>
+            ) : null}
+          </View>
 
           <View style={{ marginTop: 10, flexDirection: "row", justifyContent: "space-between" }}>
               <Button
@@ -226,17 +210,22 @@ const styles = StyleSheet.create({
     backgroundColor: "white", alignSelf: "center", width: "90%", padding: 30, borderRadius: 20
   },
   input: {
+    textAlign: 'center',
     height: 40,
+    fontSize: 20,
     borderColor: 'black',
     borderWidth: 1,
     marginBottom: 10,
     paddingHorizontal: 10,
   },
+  inputContainer: {
+    marginBottom: 10,
+  },
   error: {
     color: "red",
     padding: 0,
-    marginTop: -9,
-    marginBottom: 10,
+    marginTop: 2,
+    marginBottom: 5,
   },
   label: {
     fontSize: 16,
