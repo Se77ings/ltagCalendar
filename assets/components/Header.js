52e7423ab8300fb1d67d89ac422b58f296400ab5
se77ings <se77ings@gmail.com>
2024-10-22 08:07:30 -0300

    images added

--- assets/components/Header.js
+++ assets/components/Header.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect, useRef } from "react";
 import { View, Text, FlatList, TouchableOpacity, StyleSheet, Alert, Image, Modal, Pressable, Button, InteractionManager } from "react-native";
 import styles from "../styles/styles";
-
+import icon from "../../assets/icon.png"
 const Header = ({title}) => {
   return (
     <View style={[styles.headerContainer]}>
       <View style={styles.logoContainer}>
-        <Image source={{ uri: "https://img.freepik.com/psd-gratuitas/logotipo-abstrato-gradiente_23-2150689648.jpg" }} style={styles.logo} />
+        <Image source={icon} style={styles.logo} />
         <View >
           <Text style={styles.shopName}>LTAG CALENDAR</Text>
           <Text style={styles.shopName_}>{title}</Text>
