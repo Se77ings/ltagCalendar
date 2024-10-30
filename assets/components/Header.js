e33b2b78678c98080098ac91aac5f04db84e5c62
se77ings <se77ings@gmail.com>
2024-10-30 08:00:42 -0300

    continue..

--- assets/components/Header.js
+++ assets/components/Header.js
@@ -1,25 +1,28 @@
 import React, { useState, useEffect, useRef } from "react";
 import { View, Text, FlatList, TouchableOpacity, StyleSheet, Alert, Image, Modal, Pressable, Button, InteractionManager } from "react-native";
 import styles from "../styles/styles";
-import icon from "../../assets/icon.png"
-import { LinearGradient } from 'expo-linear-gradient';
+import icon from "../../assets/icon.png";
+import { LinearGradient } from "expo-linear-gradient";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
-const Header = ({title}) => {
+const Header = ({ title }) => {
   return (
     <View>
-      <LinearGradient
-        colors={['#000000', '#0055c6']}
-        start={{ x: 0.3, y: 0.2 }}
-        style={styles.headerContainer}>
+      <LinearGradient colors={["#000000", "#0055c6"]} start={{ x: 0.3, y: 0.2 }} style={styles.headerContainer}>
+        <View style={styles.logoContainer}>
+          <Image source={icon} style={styles.logo} />
+          <View style={{flex:1,flexDirection:"row", justifyContent:"space-around"}}>
+            <View>
+              <Text style={styles.shopName}>LTAG CALENDAR</Text>
+              <Text style={styles.shopName_}>{title}</Text>
+            </View>
+            {/* <Text style={styles.shopName_}>SEJA BEM-VINDO!!</Text> */}
+            <View style={{marginLeft:25}}>
 
-      <View style={styles.logoContainer}>
-        <Image source={icon} style={styles.logo} />
-        <View >
-          <Text style={styles.shopName}>LTAG CALENDAR</Text>
-          <Text style={styles.shopName_}>{title}</Text>
-          {/* <Text style={styles.shopName_}>SEJA BEM-VINDO!!</Text> */}
+            <Ionicons name="contrast-outline" size={25} color="white" />
+            </View>
+          </View>
         </View>
-      </View>
       </LinearGradient>
     </View>
   );
